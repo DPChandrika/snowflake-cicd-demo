f952b225a04bc638a94021e8f83e89c2088a4a3f
DPChandrika <50520736+DPChandrika@users.noreply.github.com>
2024-07-11 15:47:29 +0530

    Update sf_deploy_dev.sql

--- demo_project/sf_deploy_dev.sql
+++ demo_project/sf_deploy_dev.sql
@@ -1,2 +1,2 @@
-EXECUTE IMMEDIATE FROM @DEMO_SNOWFLAKE_REPO/branches/main/demo_project/bronze/Tables/Customer.sql USING (ENV => 'DEV');
-EXECUTE IMMEDIATE FROM @DEMO_SNOWFLAKE_REPO/branches/main/demo_project/bronze/Tables/Orders.sql USING (ENV => 'DEV');
+EXECUTE IMMEDIATE FROM @DEMO_SNOWFLAKE_REPO/branches/main/demo_project/bronze/Tables/Customer.sql;
+EXECUTE IMMEDIATE FROM @DEMO_SNOWFLAKE_REPO/branches/main/demo_project/bronze/Tables/Orders.sql;
